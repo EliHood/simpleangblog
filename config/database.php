2f5204c3db6d6fe1bbb9dbd98351386f86ff915d
POWER USER <poweruser@Users-iMac-2.local>
2017-11-25 18:35:34 -0500

    update

--- config/database.php
+++ config/database.php
@@ -1,18 +1,12 @@
 <?php
 
-if ($url = env('CLEARDB_DATABASE_URL', false)) {
-    $parts = parse_url($url);
-    $host = $parts["host"];
-    $username = $parts["user"];
-    $password = $parts["pass"];
-    $database = substr($parts["path"], 1);
-} else {
-    $host = env('DB_HOST', 'localhost');
-    $username = env('DB_USERNAME', 'forge');
-    $password = env('DB_PASSWORD', '');
-    $database = env('DB_DATABASE', 'forge');
-    $port = env('DB_PORT', '3306');
-}
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 
 return [
 
